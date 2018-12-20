8f3a947db93ffdb4f6541cc28130150cc574183e
Jalel YAKDI <jyakdi@e1r8p3.42.fr>
2018-12-20 15:57:28 +0100

    generator + start solving

--- Puzzle.py
+++ Puzzle.py
@@ -1,3 +1,5 @@
+import numpy as np
+
 class Puzzle:
     def __init__(self, tab, x, y, size):
         self.tab = tab
@@ -7,36 +9,68 @@ class Puzzle:
 
     def make_right(self):
         if (self.x > 0):
-            tab = self.tab
+            tab = self.copy_tab()
             tab[self.y][self.x] = tab[self.y][self.x - 1]
-            tab[self.y][self.x - 1] = -1
+            tab[self.y][self.x - 1] = 0
             return Puzzle(tab, self.x - 1, self.y, self.size)
         else:
-            return 0
+            return self
 
     def make_left(self):
         if (self.x < self.size - 1):
-            tab = self.tab
+            tab = self.copy_tab()
             tab[self.y][self.x] = tab[self.y][self.x + 1]
-            tab[self.y][self.x + 1] = -1
+            tab[self.y][self.x + 1] = 0
             return Puzzle(tab, self.x + 1, self.y, self.size)
         else:
-            return 0
+            return self
 
     def make_down(self):
         if (self.y > 0):
-            tab = self.tab
+            tab = self.copy_tab()
             tab[self.y][self.x] = tab[self.y - 1][self.x]
-            tab[self.y - 1][self.x] = -1
+            tab[self.y - 1][self.x] = 0
             return Puzzle(tab, self.x, self.y - 1, self.size)
         else:
-            return 0
+            return self
 
     def make_up(self):
         if (self.y < self.size - 1):
-            tab = self.tab
+            tab = self.copy_tab()
             tab[self.y][self.x] = tab[self.y + 1][self.x]
-            tab[self.y + 1][self.x] = -1
+            tab[self.y + 1][self.x] = 0
             return Puzzle(tab, self.x, self.y + 1, self.size)
         else:
-            return 0
+            return self
+
+    def print_puzzle(self):
+        print(np.matrix(self.tab))
+
+    def copy_tab(self):
+        tab = []
+        for k in range(self.size):
+            tab.append([0] * self.size)
+        for i in range(self.size):
+            for j in range(self.size):
+                tab[i][j] = self.tab[i][j]
+        return tab
+
+    def find(self, final, nb):
+        pt = []
+        for i in range(self.size):
+            for j in range(self.size):
+                if final[i][j] == nb:
+                    pt.append(i)
+                    pt.append(j)
+                    return pt
+
+    def manhattan(self, final):
+        total = 0
+        for i in range(self.size):
+            for j in range(self.size):
+                nb = self.tab[i][j]
+                if nb == 0:
+                    continue
+                pt = self.find(final, nb)
+                total = total + abs(i - pt[0]) + abs(j - pt[1])
+        return total
--- __main__.py
+++ __main__.py
@@ -1,16 +1,120 @@
 import Puzzle as puz
+import random
+#import matplotlib.pyplot as plt
+import time
+
+def create_final(size):
+    i = 0
+    j = 0
+    way = 0
+    move = False
+    tab = []
+    for k in range(size):
+        tab.append([0] * size)
+    total = size * size - 1
+    for nb in range(total):
+        move = False
+        tab[i][j] = nb + 1
+        nb = nb + 1
+        while move == False:
+            if way == 0:
+                if j >= size - 1 or tab[i][j + 1] != 0:
+                    way = (way + 1) % 4
+                    continue
+                else:
+                    j = j + 1
+                    move = True
+            if way == 1:
+                if i >= size - 1 or tab[i + 1][j] != 0:
+                    way = (way + 1) % 4
+                    continue
+                else:
+                    i = i + 1
+                    move = True
+            if way == 2:
+                if j <= 0 or tab[i][j - 1] != 0:
+                    way = (way + 1) % 4
+                    continue
+                else:
+                    j = j - 1
+                    move = True
+            if way == 3:
+                if i <= 0 or tab[i - 1][j] != 0:
+                    way = (way + 1) % 4
+                    continue
+                else:
+                    i = i - 1
+                    move = True
+    #plt.plot(tab)
+    #plt.ylabel('Label 1')
+    #plt.show()
+    return puz.Puzzle(tab, j, i, size)
+
+def tabcmp(start, final, size):
+    for i in range(size):
+        for j in range(size):
+            if start[i][j] != final[i][j]:
+                print("{} != {}".format(start[i][j], final[i][j]))
+                return False
+    return True
+
+def solve(start, final):
+    start.print_puzzle()
+    while tabcmp(start.tab, final.tab, start.size) == False:
+        time.sleep(1)
+        score = start.manhattan(final.tab)
+        state1 = start.make_right()
+        if state1.manhattan(final.tab) < score:
+            print ("right move")
+            solve(state1, final)
+        state2 = start.make_left()
+        elif state2.manhattan(final.tab) < score:
+            print ("left move")
+            solve(state2, final)
+        state3 = start.make_up()
+        elif state3.manhattan(final.tab) < score:
+            print ("up move")
+            solve(state3, final)
+        state4 = start.make_down()
+        #if state4.manhattan(final.tab) < score:
+        else:
+            print ("down move")
+            solve(state4, final)
 
 def main():
     tab = []
-    for i in range(3):
-        tab.append([0] * 3)
-    for i in range(3):
-        for j in range(3):
-            tab[i][j] = i * 3 + j
-    print tab
-    #tab[0][1] = 1
-    #puz.Puzzle()
-
+    ran = []
+    x = 0
+    y = 0
+    size = 2
+    for k in range(size):
+        tab.append([0] * size)
+    for l in range(size * size):
+        ran.append(l)
+    for i in range(size):
+        for j in range(size):
+            nb = random.choice(ran)
+            if nb == 0:
+                x = i
+                y = j
+            tab[i][j] = nb
+            ran.remove(nb)
+    start = puz.Puzzle(tab, y, x, size)
+    print("random N-Puzzle :")
+    start.print_puzzle()
+    final = create_final(size)
+    print ("\nfinal N-Puzzle :")
+    final.print_puzzle()
+    solve(start, final)
+    '''state1 = start.make_down()
+    total = start.manhattan(final.tab)
+    print ("Manhattan value = {}".format(total))
+    print ("\nState1 (move_down) :")
+    state1.print_puzzle()
+    total2 = state1.manhattan(final.tab)
+    print ("Manhattan value = {}".format(total2))
+    print ("\nfinal N-Puzzle :")
+    final.print_puzzle()'''
 
 if __name__ == "__main__":
     main()
